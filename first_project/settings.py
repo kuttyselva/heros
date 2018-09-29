7065983ff2560ca3e613a1f21e0fc3ae12993516
kuttyselva <kuttyselva57@gmail.com>
2018-09-29 14:49:55 +0530

    ss
    s

--- first_project/settings.py
+++ first_project/settings.py
@@ -118,11 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[STATIC_DIR,]
-STATIC_ROOT = os.path.join(STATIC_DIR,"staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- first_project/wsgi.py
+++ first_project/wsgi.py
@@ -12,8 +12,3 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
-
-application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
